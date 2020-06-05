727ef5494055fd0116f11688af72e34a8f3a6422
Ivan Carrasco <alexcct@hotmail.com>
2020-06-05 11:43:50 -0400

    adding form with formspree

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,4 +1,5 @@
-<?php
+ 
+ <?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
